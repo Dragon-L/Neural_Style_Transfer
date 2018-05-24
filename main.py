74533f12b0947937e4759c901b861bf31dfa3b23
Dragon-L <530593201@qq.com>
2018-05-24 10:41:01 +0800

    use vgg19 to predict object

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import keras as ks
+import tensorflow as tf
+import tensorflow.contrib.keras as ks
 
-vgg19 = ks.applications.vgg19.VGG19(weights='imagenet')
+img = ks.preprocessing.image.load_img('./images/car.jpg', target_size=(224, 224))
+img = ks.preprocessing.image.img_to_array(img)
+img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
 
+vgg19 =ks.applications.vgg19.VGG19(weights='imagenet')
+yhat = vgg19.predict(img)
+label = ks.applications.vgg19.decode_predictions(yhat)
+print(label[0][0])
 
 print('it is done')
